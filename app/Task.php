cff89c02d686c9faabf65ee477b7f055c0c277f9
David Pearson <dpearson@weebly.com>
2016-08-17 19:16:47 -0700

    Upgraded to Laravel 5.3-RC1

--- app/Task.php
+++ app/Task.php
@@ -2,11 +2,14 @@
 
 namespace App;
 
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    /**
+	use Notifiable;
+	
+	/**
      * @var array
      */
     protected $fillable = [
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'api/v1'], function () {
+    Route::resource('task', 'TaskController');
+    Route::resource('user', 'UserController');
+	Route::post('login', 'LoginController@login');
+});
