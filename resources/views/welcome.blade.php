60b77b67aca07f6de04f1de52088e418c5be000e
David Pearson <dpearson@weebly.com>
2016-08-17 19:31:18 -0700

    Changed title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>HackBoard</title>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
